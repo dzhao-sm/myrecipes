8cc91d4d536b1df20ccf85b3d99d92a5e46abd90
Delmar Zhao <dzhao@surveymonkey.com>
2020-06-02 16:41:06 -0400

    complete admin feature with authentication and tests 5-129"

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -4,6 +4,10 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "Delmar", email: "delmar@example.com",
                             password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                            password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "john", email: "admin@example.com",
+                            password: "password", password_confirmation: "password", admin: true)
   end
 
   test "reject an invalid edit" do
@@ -16,7 +20,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
 
-  test "accept a valid signup" do 
+  test "accept a valid edit" do 
     sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
@@ -26,5 +30,29 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     @chef.reload
     assert_match "DelmarZ", @chef.chefname
     assert_match "delmar.example@gmail.com", @chef.email
+  end
+
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "Delmar4", email: "delmar4.example@gmail.com"} }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Delmar4", @chef.chefname
+    assert_match "delmar4.example@gmail.com", @chef.email
+  end
+
+  test "redirect edit attempt by non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@email.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_name, email: updated_email} }
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Delmar", @chef.chefname
+    assert_match "delmar@example.com", @chef.email
   end
 end
